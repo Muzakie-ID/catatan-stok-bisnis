cfe675c737774ed837278524c0427b95ca74c905
Muzakie-ID <muzakie624@gmail.com>
2025-12-30 22:35:12 +0700

    fix: auto fix storage permissions in entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# Fix permissions for storage and cache (important when using volumes)
+chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
+chmod -R 775 /var/www/storage /var/www/bootstrap/cache
+
 # Create storage link
 php artisan storage:link
 
